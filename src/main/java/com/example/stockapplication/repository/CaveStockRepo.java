f38b9415de914432869bd893fe05de7b5b5e207e
Yoann Meynsan <yoann.meynsan@viacesi.fr>
2025-10-20 14:46:23 +0200

    Controller updated

--- src/main/java/com/example/stockapplication/repository/CaveStockRepo.java
+++ src/main/java/com/example/stockapplication/repository/CaveStockRepo.java
@@ -1,16 +1,14 @@
 package com.example.stockapplication.repository;
 
-import com.example.stockapplication.entity.CaveLogs;
 import com.example.stockapplication.entity.CaveStock;
-import com.example.stockapplication.entity.CaveSupplier;
-import com.example.stockapplication.entity.CaveUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CaveStockRepo extends JpaRepository<CaveStock, Integer>{
 
     List<CaveStock> findByGenre(String genre);
 
-    CaveStock getStockById(Integer stock_id);
+    Optional<CaveStock> findById(Integer stock_id);
 }
