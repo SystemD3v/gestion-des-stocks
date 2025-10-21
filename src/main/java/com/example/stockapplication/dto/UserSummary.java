e4b83ca373ded3803ffa25ff2619360ec52074df
LucasRaoul <lucas.raoul.pro@gmail.com>
2025-10-21 14:37:20 +0200

    fix merging problem

---
+++ src/main/java/com/example/stockapplication/dto/UserSummary.java
@@ -0,0 +1,18 @@
+package com.example.stockapplication.dto;
+
+public class UserSummary {
+    private Integer user_id;
+
+    private String lastname;
+
+    private String firstname;
+
+
+    private Integer total_bottles_bought;
+    private String email;
+    private String password;
+    private String role;
+    private String address;
+    private String phone_number;
+
+}
