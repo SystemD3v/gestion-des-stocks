97e89a2ce0b392524a2b8b62f9bc4a6518e2787b
LucasRaoul <lucas.raoul.pro@gmail.com>
2025-10-21 15:46:08 +0200

    add javaFx folder

---
+++ JavaFX/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javafx to javafx.fxml;
+    exports com.example.javafx;
+}
