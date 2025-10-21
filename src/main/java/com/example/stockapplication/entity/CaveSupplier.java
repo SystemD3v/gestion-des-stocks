3b1e21e7024762cd0af77953cc0644deaadd99b2
trap2game <dutilhtom@gmail.com>
2025-10-21 11:33:12 +0200

    suppliergetbyid

--- src/main/java/com/example/stockapplication/entity/CaveSupplier.java
+++ src/main/java/com/example/stockapplication/entity/CaveSupplier.java
@@ -16,7 +16,7 @@ public class CaveSupplier {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "supplier_id")
 
-    private Integer supplier_id;
+    private Integer id;
     private String supplier_name;
     private String supplier_address;
     private Integer supplier_phone;
