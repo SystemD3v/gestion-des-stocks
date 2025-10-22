666508c2d7c4e698567e7aec4295b68c2693fcb1
LucasRaoul <lucas.raoul.pro@gmail.com>
2025-10-22 11:44:07 +0200

    layout

--- JavaFX/src/main/java/Controller/HelloController.java
+++ JavaFX/src/main/java/Controller/HelloController.java
@@ -1,14 +1,16 @@
-package com.example.javafx;
+package Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.awt.event.ActionEvent;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void btn(ActionEvent event) {
+
     }
 }
