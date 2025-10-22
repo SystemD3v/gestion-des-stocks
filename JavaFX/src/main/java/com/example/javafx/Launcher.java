ba3aa286cc72a3ce11457afb7f4d4a2ec8cf053f
Yoann Meynsan <yoann.meynsan@viacesi.fr>
2025-10-22 14:08:55 +0200

    MVC architecture

--- JavaFX/src/main/java/com/example/javafx/Launcher.java
+++ JavaFX/src/main/java/com/example/javafx/Launcher.java
@@ -1,9 +1,10 @@
 package com.example.javafx;
 
+import com.example.javafx.view.Dashboard;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(Dashboard.class, args);
     }
 }
--- JavaFX/src/main/java/com/example/javafx/view/Dashboard.java
+++ JavaFX/src/main/java/com/example/javafx/view/Dashboard.java
@@ -1,4 +1,4 @@
-package com.example.javafx;
+package com.example.javafx.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Dashboard extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("dashboard-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("Dashboard");
         stage.setScene(scene);
         stage.show();
     }
